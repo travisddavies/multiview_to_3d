88d209e49ae7b58a738f7946584a14c6915ca6dd
travisddavies <travisddavies@gmail.com>
2024-03-08 21:29:15 +0800

    Added the Bottleneck block

---
+++ blocks.py
@@ -0,0 +1,53 @@
+import torch.nn as nn
+
+BN_MOMENTUM = 0.1
+
+
+class Bottleneck(nn.Module):
+    """
+    The block basically is a three conv block combination, which contains
+    a residual which if downsampling occurs will use a residual conv block
+    to remember information from the previous layer, otherwise uses the
+    original output from the previous layer.
+
+    Bottlenecking occurs in the middle conv block, where the stride and the
+    kernel size are larger
+    """
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1,
+                               bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion,
+                                  momentum=BN_MOMENTUM)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        else:
+            residual = x
+
+        out += residual
+        out = self.relu(out)
+
+        return out
